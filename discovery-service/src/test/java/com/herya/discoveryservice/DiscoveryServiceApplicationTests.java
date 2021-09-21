e33fd72062ce4dac31d4cdf2902d15b67ff49a0c
Alfan Herya <alfanherya@gmail.com>
2021-09-21 07:02:02 +0700

    discovery service

---
+++ discovery-service/src/test/java/com/herya/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.herya.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
